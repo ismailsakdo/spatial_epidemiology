7ece6db0b18e701d6a7ee0278f6db90273e08bde
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-06-30 11:13:06 +0800

    Create filterWeek1till4.py

---
+++ filterWeek1till4.py
@@ -0,0 +1,23 @@
+from qgis.core import QgsProject, QgsVectorLayer
+
+# Set the paths to your shapefiles
+polygon_file = '/Users/ismailsa/Downloads/0.AaaaGIS/SabahNew/dataCase/caseFile.shp'
+point_file = '/Users/ismailsa/Downloads/0.AaaaGIS/SabahNew/dataCase/dummyCase1.shp'
+
+# Load the polygon layer
+polygon_layer = QgsVectorLayer(polygon_file, 'Polygon Layer', 'ogr')
+if not polygon_layer.isValid():
+    print('Failed to load polygon layer:', polygon_layer.error().message())
+
+# Load the point layer
+point_layer = QgsVectorLayer(point_file, 'Point Layer', 'ogr')
+if not point_layer.isValid():
+    print('Failed to load point layer:', point_layer.error().message())
+
+# Add the layers to the project
+QgsProject.instance().addMapLayer(polygon_layer)
+QgsProject.instance().addMapLayer(point_layer)
+
+# Filter the point layer for Week 1 to Week 4
+expression = "\"Week\" >= 1 AND \"Week\" <= 4"
+point_layer.setSubsetString(expression)
